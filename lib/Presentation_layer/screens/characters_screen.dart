efc2a31f0799bb9b26053fe44b655cfd5d5b57af
Youssef-Wheed <wayoussef138@gmail.com>
2026-07-28 05:36:13 +0300

    run BloC BlocProvider

--- lib/Presentation_layer/screens/characters_screen.dart
+++ lib/Presentation_layer/screens/characters_screen.dart
@@ -4,6 +4,17 @@ class CharactersScreen extends StatelessWidget {
   const CharactersScreen({super.key});
 
   @override
+  State<CharactersScreen> createState() => _CharactersScreenState();
+}
+
+class _CharactersScreenState extends State<CharactersScreen> {
+ late List<Character> allCharacters;
+  @override
+
+  void initState(){
+    super.initState();
+    allCharacters = BlocProvider.of<CharacterCubit>(context).getAllCharacters();
+  }
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
